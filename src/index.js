0e8c588bb70943251f3a3e3324afb20c31380337
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2021-12-16 00:59:55 +0530

    Update refresh button functionality

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import recieveData from './utils/post.js';
 const refreshBtn = document.querySelector('.refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
-  window.location.reload();
+  const ul = document.querySelector('.score-list');
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+  }
+  renderData();
 });
 
 renderData();
