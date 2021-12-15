f888d1acf1093a87b5beb40931ba544d50a4d34b
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2021-12-15 21:33:34 +0530

    Add get data from api functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './style.css';
+import { renderData } from './utils/get';
 
 const refreshBtn = document.querySelector('.refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
 window.location.reload();
 });
+
+renderData();
---
+++ src/utils/get.js
@@ -0,0 +1,18 @@
+async function getData() {
+ const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rO378WlzTyLm1i3wuLMC/scores/')
+ const json = await response.json();   
+    return json;
+}
+
+const renderData=() => {
+    const ul= document.querySelector('.score-list');
+    getData().then(json => {
+        const array= json.result;
+        for(let i=0; i<array.length; i++){
+        ul.innerHTML +=`<li class="score">${array[i].user} ${array[i].score}</li>`;
+    }
+    });
+}
+
+
+export {renderData};
